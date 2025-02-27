cbebb4b010122df3a4b2fea0cf2a0afaf614e5dd
Abdul-lm-10 <abdulrazeethu2001@gmail.com>
2025-02-27 22:50:54 +0530

    test-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Layout from './Component/Layout'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Component/Layout";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout/>}/>
-        
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Layout />} />
+        </Routes>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
